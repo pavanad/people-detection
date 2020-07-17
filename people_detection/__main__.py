d5aabf2bc6ddac806d35e4e2e0645d38546ca64e
Adilson Pavan <adilson.pavan@ies2.com.br>
2020-07-17 12:44:47 -0300

    bug fix and set rtsp transport for udp

--- people_detection/__main__.py
+++ people_detection/__main__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import os
 import time
 
 import cv2
-
 from config.settings import RTSP_URL
 from services.detection import PeopleDetection
 
@@ -13,16 +13,19 @@ def main():
 
     people = PeopleDetection()
 
-    cap = cv2.VideoCapture(RTSP_URL)
+    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp"    
+    cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
     if not cap.isOpened():
         return
 
-    while True:
+    while True:        
         ret, frame = cap.read()
         detected = people.detect(frame)
-        cv2.imshow("Social", detected)
+        cv2.imshow("Social", detected)        
         cv2.waitKey(1000)
 
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
